3fdb2ae0f567804e005216a7cab6d6a25c288979
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2022-06-09 01:24:46 +0530

    Project Setup done

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import ChatIcon from "./components/ChatIcon";
+import ChatWindow from "./components/ChatWindow";
+
+function App() {
+  return (
+    <div className="App">
+      <ChatIcon />
+      <ChatWindow />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/ChatWindow.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../App.css';
+
+
+function ChatWindow() {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default ChatWindow;
