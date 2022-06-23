989cd2c01a3f0651ff5354109bb77d2d01edd0a2
Mrityunjay383 <mrityunjayvyasmhl@gmail.com>
2022-06-23 14:41:02 +0530

    Databse Working and mobile full screen Done

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -3,13 +3,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAScfKtiqwytYiIB_2rzhcEuBmqE67mGGw",
-  authDomain: "viraj-chatbot.firebaseapp.com",
-  projectId: "viraj-chatbot",
-  storageBucket: "viraj-chatbot.appspot.com",
-  messagingSenderId: "421610551439",
-  appId: "1:421610551439:web:f1f6a630861574dfbf58fa",
-  measurementId: "G-CXRBVXTNML"
+  apiKey: "AIzaSyCgu6Ca2-hsCE9pITWrhaUiQpTl6oqyies",
+  authDomain: "vaj-chat.firebaseapp.com",
+  databaseURL: "https://vaj-chat-default-rtdb.firebaseio.com",
+  projectId: "vaj-chat",
+  storageBucket: "vaj-chat.appspot.com",
+  messagingSenderId: "768327563726",
+  appId: "1:768327563726:web:82f3b008be93992af2d39f"
 };
 
 export const app = initializeApp(firebaseConfig);
